599a20d574edcab7dc8b225fa28cf923356b6d61
Apple <Apple@apples-macbook-air.local>
2018-11-10 19:49:36 +0900

    Initial Commit

---
+++ WinPath/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WinPath
+//
+//  Created by Apple on 2018/11/10.
+//  Copyright © 2018 Kazumi Watanabe. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
